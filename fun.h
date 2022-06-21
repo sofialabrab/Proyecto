a2236621f43da5ec353f7175ea128ba0b96f3932
Repollo1000 <sebastian.soza.m@mail.pucv.cl>
2022-06-21 18:49:57 -0400

    commit seba
    Probando los commit

--- fun.h
+++ fun.h
@@ -9,24 +9,44 @@ typedef struct{
  int fichasEnBase;
 
 }tipoEstado;
-
+typedef struct{
+  int posicion1;
+  int posicion2;
+  int posicion3;
+  int posicion4;  
+}tipoPosicionFicha;
 typedef struct
 {
   char color[10];
   tipoEstado fichas;
   int turno;
   bool activo;
+  tipoPosicionFicha posiciones;
+  
 }tipoJugador;
+
+typedef struct
+{
+  int amarillo;
+  int azul;
+  int rojo;
+  int verde;
+  int posicion_arreglo;
+}tipoRecorrido;
 
 //void VisualizacionTablero(List *);
 int lanzarDado();
-void ModalidadTradicional(List *, char* arreglo[225],tipoJugador* arregloTurno); 
+void ModalidadTradicional(List *, char* arreglo[225],tipoJugador* arregloTurno, tipoRecorrido*); 
 //void ModalidadAlternativa(List *); 
 void imprimirMatriz(char* matriz[15][15],int);
 void imprimirTablero (char* arreglo[225]);
-void mostrarTurno(int , int );
+void mostrarTurno(int , int, tipoJugador* );
 void enJuego();
 void consultaGanador();
-int primerJugador(int);
+int primerJugador(tipoJugador *,int);
 void esGanador();
-void EnJuego(char * arreglo[225], List *, int);
+void EnJuego(char * arreglo[225], List *, int, tipoJugador*,tipoRecorrido*);
+void crearRecorrido(tipoRecorrido* arregloRecorrido);
+void editarTablero(int, int, char* arreglo[225], tipoJugador*, tipoRecorrido*, int);
+void crearTablero(char* arreglo[225]);
+int turno(tipoJugador *,int );
--- main.c
+++ main.c
@@ -3,22 +3,26 @@
 #include "list.h"
 #include "fun.h"
 #include <math.h>
+#include <string.h>
 
 int main(void) 
 {
   char* matriz[15][15] = {};
   char* arreglo[225];
+  crearTablero(arreglo);
+  tipoRecorrido* arregloRecorrido = malloc(52*sizeof(tipoRecorrido));
+  crearRecorrido(arregloRecorrido);
   tipoJugador* arregloTurnos = calloc(4, sizeof(tipoJugador)); //0-Rojo,1-Azul,2-Verde,3-Amarillo
   int modalidad;
   List *listJugadores = createList(); 
-  lanzarDado();
+  //lanzarDado();
   printf("MENÚ\n\n");
   printf("Modalidades:\n");
   printf("1.- Modalidad Tradicional\n (Partida tradicional, cada jugador jugara con un color y sus 4 fichas, ademas de un dado de 6 caras)\n\n");
   printf("2.- Modalidad Alternativa\n(Partida Alternativa, cada jugador jugara con un personaje asociado a un color, sus 4 fichas y su dado  especial de 8 caras, el cual contiene una ventaja y un castigo) \n\n");
   printf("Elija su modalidad de juego: ");
   scanf("%d", &modalidad);
-  if(modalidad == 1) ModalidadTradicional(listJugadores, arreglo,arregloTurnos);  
+  if(modalidad == 1) ModalidadTradicional(listJugadores, arreglo,arregloTurnos, arregloRecorrido);  
   //else ModalidadAlternativa();
   //imprimirMatriz(matriz);
   //imprimirTablero(arreglo);
